4aa39942d4e9466b4898c02c231400aeaefc2378
João Pedro A <jaaopbr@gmail.com>
2024-02-27 13:23:06 -0300

    feat: add rng command
    rng command generates a random string of length N or 9 by default.

--- main.go
+++ main.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"math/rand"
+	"os"
+	"strconv"
+	"github.com/urfave/cli/v2"
+)
 
 func main() {
-  fmt.Println("Hello, World!")
+  app := &cli.App{
+    Name: "rnd",
+    Usage: "Generate a random 9 characters string",
+    Args: true,
+    ArgsUsage: "Lenght of the random string",
+    Action: func(ctx *cli.Context) error {
+      bytes := []byte("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@.?");
+      length, err := strconv.Atoi(ctx.Args().Get(1))
+
+      if length > 100 {
+        log.Fatal("Lenght must be lower than 100")
+      }
+
+      if err != nil {
+        length = 9
+      }
+
+      pwd := make([]byte, length)
+      for i := range pwd {
+        pwd[i] = bytes[rand.Intn(len(bytes)-1)]
+      }
+      fmt.Println(string(pwd))
+      return nil
+    },
+  }
+
+  if err := app.Run(os.Args); err != nil {
+    log.Fatal(err)
+  }
 }
